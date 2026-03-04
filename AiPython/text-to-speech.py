25f3e02b4625afadd2458761d31c43f7d50abca1
nixveil <iamnixveil@gmail.com>
2026-03-04 20:09:53 +0100

    Training Ai models

--- AiPython/text-to-speech.py
+++
@@ -1,5 +0,0 @@
-# Vosk library is used in python for speech-to-text
-# To install the library use <pip install vosk> in your terminal
-# Furthermore you will need to download a pre-trained language model(eg.english) 
-# From the website unzip it into a local directory 
- 
