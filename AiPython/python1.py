c6d7fb5de41d7b92481230a0f8d12b6bd64dd63f
nixveil <iamnixveil@gmail.com>
2026-03-05 00:54:19 +0100

    Training Ai models

--- AiPython/python1.py
+++ AiPython/python1.py
@@ -1,9 +1,17 @@
+import random as rdm
+import math
+
 list1 = [1,2,3,4,5,6,7,8,9,10]
 list2 = [2,4,6,8,10,12,14,16,18,20]
 
-weight = int(input("enter any number"))
+weight = rdm.randint(0,1)
+for i in range(100):
+    guess = [weight * x for x in list1]
 
-guess = [weight * x for x in list1]
-
-
-print(guess)
+    checkGuess = [j - i for j,i in zip(list2,guess)]
+    FinalGuess = [x ** 2 for x in checkGuess]
+    for add in FinalGuess:
+        add+=add
+        AverageGuess = add/len(FinalGuess)
+        error = weight - (0.01 *AverageGuess )
+print(error)
