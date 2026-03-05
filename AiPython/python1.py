87246f969b167006eece9c3caaab9045c792265f
nixveil <iamnixveil@gmail.com>
2026-03-05 01:02:41 +0100

    Training Ai models

--- AiPython/python1.py
+++ AiPython/python1.py
@@ -4,14 +4,16 @@ import math
 list1 = [1,2,3,4,5,6,7,8,9,10]
 list2 = [2,4,6,8,10,12,14,16,18,20]
 
-weight = rdm.randint(0,1)
+weight = rdm.uniform(0.1,1.5)
 for i in range(100):
     guess = [weight * x for x in list1]
 
     checkGuess = [j - i for j,i in zip(list2,guess)]
     FinalGuess = [x ** 2 for x in checkGuess]
-    for add in FinalGuess:
-        add+=add
-        AverageGuess = add/len(FinalGuess)
-        error = weight - (0.01 *AverageGuess )
-print(error)
+    TotalfinalGuess = sum(FinalGuess)
+    AverageGuess = TotalfinalGuess/len(FinalGuess)
+    
+    checkGuess = [ i -  j for i,j in zip(list2,guess)]
+    weight = weight - (0.01 *checkGuess )
+for count10 in i == 10: 
+    print(count10,weight)
